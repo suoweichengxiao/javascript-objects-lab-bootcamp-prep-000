1c925d0e115badf3ab85777a6cd9da5a37dc89b2
Joe Dettmar <joedettmar@gmail.com>
2017-11-04 18:49:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const recipes = {}
 
 function updateObjectWithKeyAndValue(object, key, value) {object[key] = value; return object};
 
+function updateObjectWithKeyAndValue(object, key, value) {var newObj = Object.assign({}, object); return newObj;};
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {object[key] = value; return object};
 
